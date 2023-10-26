4c0baa354b14f8ab2b65e287a3a4a72d29a93f31
Shay Sommerfield <shaysommerfield@gmail.com>
2023-10-25 17:39:53 -0700

    audio!

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import pygame
 import math
 from pywizlight import discovery, PilotBuilder, wizlight
 from time import sleep
@@ -17,21 +18,20 @@ async def detect_lights(bulbs):
 		await light.turn_on()
 		sleep(1)
 		
-async def ossciliate(lights, seconds_per_cycle):
+async def ossciliate(lights, seconds_per_cycle, duration):
 	start_time = time.time()
-	num_sec = 10
-	val_per_sec = 255/num_sec
 	hz = 1/seconds_per_cycle
 	omega = hz*2*math.pi
 
-	while((time.time() - start_time) < num_sec):
+	while((time.time() - start_time) < duration):
 		dt = time.time() - start_time
-		# sin wave between 0 and 1
-		val = int(255*(math.sin(omega*dt)*1/2 + 1/2))
+		# sin wave between 0 and 255
+		val = max(int(255*(math.sin(omega*dt)*1/2 + 1/2)), 1)
 		print(val)
 		await asyncio.gather(*[light.turn_on(PilotBuilder(warm_white=val)) for light in lights])
+		# sleep(0.05)
 
-async def linear_turn_on(lights, duration):
+async def linear_turn_on(lights,duration):
 	
 	await asyncio.gather(*[light.turn_off() for light in lights])
 	sleep(0.5)
@@ -42,32 +42,32 @@ async def linear_turn_on(lights, duration):
 	while((time.time() - start_time) < duration):
 		dt = time.time() - start_time
 		val = int(dt*val_per_sec)
-		if first_val:
-			first_val = val
-			print(first_val)
+		print(first_val)
 		# sin wave between 0 and 1
 		await asyncio.gather(*[light.turn_on(PilotBuilder(warm_white=val)) for light in lights])
 
-async def toggle(lights):
+async def toggle(lights,duration=1):
 	for light in lights:
 		print(light.ip)
 		await asyncio.gather(*[light.turn_on() for light in lights])
-		sleep(1)
+		sleep(duration)
 		await asyncio.gather(*[light.turn_on() for light in lights])
-		sleep(1)
+		sleep(duration)
 
 async def main():
-	bulbs = await discovery.discover_lights(broadcast_space="192.168.71.255")
+	# bulbs = await discovery.discover_lights(broadcast_space="192.168.71.255")
 
-	lights = []
-	for ip in desk_light_ips:
-		light = wizlight(ip)
-		lights.append(light)
+	pygame.mixer.init()
+	pygame.mixer.music.load('/home/pi/ding.wav')
+	pygame.mixer.music.play()
+	# while pygame.mixer.music.get_busy() == True:
+	# 	continue
 
-	await ossciliate(lights, 3)
-
-	# await asyncio.gather(*[light.turn_off() for light in lights])
-	# await asyncio.gather(lights[0].turn_off(), lights[1].turn_off())
+	# lights 
+	lights = [wizlight(ip) for ip in desk_light_ips]
+	
+	t = np.linspace()
+	await ossciliate(lights, 10, float("inf"))
 
 
 loop = asyncio.get_event_loop()
