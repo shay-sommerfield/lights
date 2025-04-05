45ebdf421134a24ec674252537c43cd41c9cbbe0
Madeline Kelly <madeline.kelly196@gmail.com>
2025-04-04 17:35:20 -0700

    Added backend proxy path

--- react_client/vite.config.js
+++ react_client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
